7ce2387349491a07f5a6df419162ee89cf6f26f6
amberocia <109210656+amberociaj@users.noreply.github.com>
2022-11-17 01:47:24 +0200

    added nightmare

---
+++ GD_GameProject/Assets/Game/Scripts/Dragon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dragon : MonoBehaviour
+{
+    public int HP = 100;
+    public Slider healthBar;
+    public Animator animator;
+
+    public void TakeDamage (int damageAmount)
+
+    {
+        HP -= damageAmount;
+        if( HP <= 0)
+        {
+            animator.SetTrigger("die");
+            GetComponent<Collider>().enabled = false;
+        }
+        else
+        {
+            animator.SetTrigger("damage");
+        }
+    }
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = HP;  
+    }
+}
