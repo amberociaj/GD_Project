434df4200a63151e04d27a9d5923a208892e9864
amberocia <109210656+amberociaj@users.noreply.github.com>
2022-11-17 02:44:56 +0200

    Enemy death
    Completed death animations for dragon and added health to dragon
    
    bullet damages enemy

--- GD_GameProject/Assets/Game/Scripts/BullletProjectile.cs
+++ GD_GameProject/Assets/Game/Scripts/BullletProjectile.cs
@@ -5,34 +5,46 @@ using UnityEngine;
 public class BullletProjectile : MonoBehaviour
 {
     private Rigidbody bulletRigidBody;
-    [SerializeField] private Transform vfxHitGreen;
-    [SerializeField] private Transform vfxHitRed;
+    //[SerializeField] private Transform vfxHitGreen;
+    //[SerializeField] private Transform vfxHitRed;
+    public int damageAmount = 20;
 
     private void Awake()
     {
         bulletRigidBody = GetComponent<Rigidbody>();
 
+
     }
 
     private void Start()
     {
+
         float speed = 40f;
         bulletRigidBody.velocity = transform.forward * speed;
+        Destroy(gameObject, 5);
 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if ( other.GetComponent<BulletTarget>() != null)
-        {
+       // if ( other.GetComponent<BulletTarget>() != null)
+       // {
             //hit target
             //Instantiate(vfxHitGreen, transform.position, Quaternion.identity);
-        }
-        else
-        {
+       // }
+       // else
+       // {
             //hit something else
             //Instantiate(vfxHitRed, transform.position, Quaternion.identity);
+       // }
+       // Destroy(gameObject);
+
+
+        Destroy(transform.GetComponent<Rigidbody>());
+        if (other.tag == "Enemy")
+        {
+            transform.parent = other.transform;
+            other.GetComponent<Dragon>().TakeDamage(damageAmount);
         }
-        Destroy(gameObject);
     }
 }
